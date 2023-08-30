ef626f75610b471788bceb047abbf7f80929e74b
sains1 <nathan.sains@gmail.com>
2023-08-30 11:50:57 +0100

    Activity Mock generator

---
+++ src/Samples/DebugTestSample/UnitTest1.cs
@@ -0,0 +1,33 @@
+using NSubstitute;
+using Temporalio.Activities;
+using Temporalio.Generators.Workflows;
+
+namespace DebugTestSample;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var activities = new TestMocks3();
+        
+        activities.MockRunAsync().Returns(Task.FromResult("goodbye"));
+
+        var result = await activities.RunAsync();
+
+    }
+}
+
+[GenerateNSubstituteMocks]
+public partial class TestMocks3 : ActivityMockBase<TestActivities>
+{
+}
+
+public class TestActivities
+{
+    [Activity]
+    public Task<string> RunAsync()
+    {
+        return Task.FromResult("hello");
+    }
+}
